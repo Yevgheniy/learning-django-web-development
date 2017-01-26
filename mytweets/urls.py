eb81c149443410729987935b435c62a3bcbc74cf
Ievgenii Sakhno <esakhno@yandex.ru>
2017-01-27 00:36:53 +0200

    Новое подтверждение

--- mytweets/urls.py
+++ mytweets/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from tweets.views import Index
 
+admin.autodiscover()
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'mytweets.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', Index.as_view()),
     url(r'^admin/', include(admin.site.urls)),
 ]

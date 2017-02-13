cdc5b4a3a6a5b18cf79bb00acc15e7ed47b855fe
Ievgenii Sakhno <esakhno@yandex.ru>
2017-02-13 22:59:24 +0200

    Tested version: user profile and tweets.

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from models import Tweet
+from models import Tweet, HashTag
+# Register your models here,
 admin.site.register(Tweet)
+admin.site.register(HashTag)
 
 
 def __unicode__(self):
